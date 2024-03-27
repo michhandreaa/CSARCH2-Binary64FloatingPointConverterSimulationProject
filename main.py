ee2e1dc5f3bed0d759c53d80de3bb496bf946c17
michhandreaa <91170055+michhandreaa@users.noreply.github.com>
2024-03-27 11:49:06 +0800

    emtpy gui almost done just dont know how to remove the bg for the frames and text :((

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import tkinter as tk
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from PIL import Image, ImageTk
+from pathlib import Path
 from conversion import ConversionSimulatorLogic
 
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\3515\Downloads\csarch2\CSARCH2-Binary64FloatingPointConverterSimulationProject\build\assets\frame0")
+
+ # Defining function to convert relative path to absolute path
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
 class ConversionSimulatorGUI(tk.Tk):
+
     def __init__(self):
         super().__init__()
 
         # initialize GUI
         self.title("CSARCH2 X22 Simulation Project")
         self.geometry("600x500")
+        self.configure(bg = "#FFFFFF")
 
         # Fixed window size
         self.resizable(width=False, height=False)
@@ -17,18 +29,34 @@ class ConversionSimulatorGUI(tk.Tk):
 
     def create_widgets(self):
 
+        # Create Canvas and Load Background Images
+        canvas = Canvas(self, height=500, width=600, bd=0, highlightthickness=0, relief="ridge")
+        canvas.place(x=0, y=0)
+
         # main label
         label = tk.Label(self, text="Binary 64 Floating Point Converter", font=("Helvetica", 16))
-        label.pack(pady=10)
+        label.pack(side=tk.TOP, pady=30)
+
+        self.background_image = ImageTk.PhotoImage(file=relative_to_assets("image_1.png"))
+        image_1 = canvas.create_image(301.0, 301.0, image=self.background_image)
+
+        self.image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
+        image_2 = canvas.create_image(385.0, 297.0, image=self.image_2)
+
+        self.image_3 = PhotoImage(file=relative_to_assets("image_3.png"))
+        image_3 = canvas.create_image(92.0, 297.0, image=self.image_3)
+
+        self.image_4 = PhotoImage(file=relative_to_assets("image_4.png"))
+        image_4 = canvas.create_image(300.0, 53.0, image=self.image_4)
 
         # left div - for inputs
-        input_frame = tk.Frame(self)
-        input_frame.pack(side=tk.LEFT, padx=10)
+        input_frame = tk.Frame(self, bg='')  # Set background color to transparent
+        input_frame.pack(side=tk.LEFT, padx=25)
 
         # input label and box
-        input_label = tk.Label(input_frame, text="Input:") #label
+        input_label = tk.Label(input_frame, text="Input:")  # label
         input_label.pack()
-        self.input_entry = tk.Entry(input_frame) #textbox
+        self.input_entry = tk.Entry(input_frame)  # textbox
         self.input_entry.pack()
 
         tk.Label(input_frame).pack()  # Empty label for padding
@@ -37,7 +65,7 @@ class ConversionSimulatorGUI(tk.Tk):
         self.step_by_step_var = tk.BooleanVar()
         step_by_step_checkbox = tk.Checkbutton(input_frame, text="Step-by-Step", variable=self.step_by_step_var)
         step_by_step_checkbox.pack()
-        
+
         # Adding padding
         tk.Label(input_frame).pack()  # Empty label for padding
 
@@ -50,20 +78,20 @@ class ConversionSimulatorGUI(tk.Tk):
         reset_button.pack(side=tk.LEFT, padx=10)
 
         # right div - for outputs
-        result_frame = tk.Frame(self)
-        result_frame.pack(side=tk.RIGHT, padx=10)
+        result_frame = tk.Frame(self, bg='')  # Set background color to transparent
+        result_frame.pack(side=tk.RIGHT, padx=20)
 
         # Binary output
         binary_output_label = tk.Label(result_frame, text="Binary:")
         binary_output_label.pack()
-        
+
         # Hexadecimal Output
         hex_output_label = tk.Label(result_frame, text="Hexadecimal:")
         hex_output_label.pack()
 
         # scrollbar for outputs of step-by-step
         scrollbar = tk.Scrollbar(result_frame, orient=tk.VERTICAL)
-        self.result_text = tk.Text(result_frame, height=20, width=55, yscrollcommand=scrollbar.set)
+        self.result_text = tk.Text(result_frame, height=17, width=45, yscrollcommand=scrollbar.set)
         scrollbar.config(command=self.result_text.yview)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.result_text.pack()
@@ -73,6 +101,7 @@ class ConversionSimulatorGUI(tk.Tk):
         # Print to Txt File output
         save_button = tk.Button(result_frame, text="Save to File", command=self.save_to_file)
         save_button.pack()
+
 
     def run_simulation(self):
         # Method to run the simulation
