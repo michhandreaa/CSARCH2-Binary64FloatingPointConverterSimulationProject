15fbe71f4e01b8a95c2560622fd3feb4d2bdf651
michhandreaa <91170055+michhandreaa@users.noreply.github.com>
2024-04-08 18:57:30 +0800

    fixed normalization

--- conversion.py
+++ conversion.py
@@ -76,55 +76,31 @@ class ConversionSimulatorLogic:
 
         return converted_exponent
     
-
+# DONE
     def normalize(mantissa,  exponent):
         # ------------ NORMALIZE ------------
 
-        new_mantissa = ""
-
-        # Step 1: Look for '.'
-        dot_index = mantissa.find('.')
-
-        # If no '.', add one to the right most part of the string
-        if '.' not in mantissa:
-            mantissa += '.'
-
-        # Step 2: Check if '1.' is in the leftmost part of the string
-        if not mantissa.startswith('1.'):
-            found_target = False
+        exponent = int(exponent)
+        new_mantissa = mantissa.replace('.', '')
+        
+        #Find the index of the first '1' in the mantissa
+        one_index = new_mantissa.find('1')
+        
+        #Move the decimal point to just after this '1'
+        new_mantissa = new_mantissa[:one_index+1] + '.' + new_mantissa[one_index+1:]
+        
+        #Adjust the exponent based on the movement of the decimal point
+        if mantissa[0] == '-':
+            dot_index = mantissa.find('.') - 1
             
-            whole, fraction = str(mantissa).split(".")
-
-            mantissa = whole + fraction
+        else:
+            dot_index = mantissa.find('.')
             
-            ''' Step 3a: check if there is a '1" anywhere on left side portion of current whole part.
-                        if there is, iterate through each char until encounter FIRST "1" then insert a "."
-            '''            
-            for index, one in enumerate(whole):
-                #check left:
-                new_mantissa += one 
-                if one == "1" and not found_target:
-                    new_mantissa += "."
-                    found_target = True
-
-            if found_target == True:
-                new_mantissa = new_mantissa+fraction
-
-            # Step 4a: If new "." was inserted to whole, add 
-            exponent = (dot_index-index) + exponent
-            
-            ''' Step 3b: check if there is a '1" anywhere on right side portion of current fraction part.
-                        if there is, iterate through each char until encounter FIRST "1" then insert a "."
-            '''     
-            if not found_target:
-                for index, one in enumerate(fraction):
-                    new_mantissa += one
-                    if one == "1" and not found_target:
-                        new_mantissa += "."
-                        # Step 4a: If new "." was inserted in fraction, subtract 
-                        exponent = (dot_index-index) - exponent
-                        found_target = True
-
+        exponent += dot_index - (one_index+1)
+        
+        #Remove any leading or trailing zeroes
+        new_mantissa = new_mantissa.strip('0')
+        
         return new_mantissa, exponent
 
 
